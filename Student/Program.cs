0ccf1242f68b100bd37fa15902d37814fc05f447
Raywire <ryanwire@outlook.com>
2019-10-04 16:54:42 +0300

    Searching Sorted Lists

--- Student/Program.cs
+++ Student/Program.cs
@@ -15,6 +15,17 @@ namespace Treehouse
 
           students.Sort();
 
+          Student newStudent = new Student() { Name = "Joe", GradeLevel = 2 };
+
+          int index = students.BinarySearch(newStudent);
+
+          if(index < 0)
+          {
+            // students.Insert((-index) - 1, newStudent);
+            // This is known as taking the bitwise complement of an integer we can use the tilde(bitwise operator) symbol
+            students.Insert(~index, newStudent);
+          }
+
           foreach (Student student in students)
           {
             System.Console.WriteLine($"{student.Name} is in grade: {student.GradeLevel}");
