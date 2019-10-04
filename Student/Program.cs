a5a1bb119a42d9ed6d797ae9eb52b7666d10f315
Raywire <ryanwire@outlook.com>
2019-10-04 17:02:32 +0300

    Collection Interfaces We can make our code easier to use by using collection interfaces whenever possible

--- Student/Program.cs
+++ Student/Program.cs
@@ -13,18 +13,6 @@ namespace Treehouse
             new Student() { Name = "Sally", GradeLevel = 2},
           };
 
-          students.Sort();
-
-          Student newStudent = new Student() { Name = "Joe", GradeLevel = 2 };
-
-          int index = students.BinarySearch(newStudent);
-
-          if(index < 0)
-          {
-            // students.Insert((-index) - 1, newStudent);
-            // This is known as taking the bitwise complement of an integer we can use the tilde(bitwise operator) symbol
-            students.Insert(~index, newStudent);
-          }
 
           foreach (Student student in students)
           {
